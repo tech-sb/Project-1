65dba2bc41190a9a47da0291f1c61d8c7eefee22
SBWebDev <simon@techni-kool.com>
2012-01-09 02:49:55 +0000

    init project version

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+	<?php wp_reset_query(); ?>
+
+	<div id="content" class="clearfix">	
+
+			<h3 class="page-title"><span>404</span></h3>
+
+			<div class="page-content wide">
+	
+			Sorry we can't find what you are looking for. Try and navigate using the main menu above.
+		
+			</div>
+	
+	</div>
+
+<?php get_footer(); ?>
